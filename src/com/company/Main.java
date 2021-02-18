b22de1249083d5c43e0509164a4659578f7edcd2
Boldeusz <mnpower@poczta.onet.pl>
2021-02-18 13:07:04 +0100

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,25 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	Customer customer = new Customer("Marcin", 12.45);
-	Customer anotherCustomer;
-	anotherCustomer = customer;
-	anotherCustomer.setBalance(34.56);
+        Customer customer = new Customer("Marcin", 12.45);
+        Customer anotherCustomer;
+        anotherCustomer = customer;
+        anotherCustomer.setBalance(34.56);
         System.out.println("Balance for customer " + customer.getName() + " is " + customer.getBalance());
+
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+
+        intList.add(1);
+        intList.add(3);
+        intList.add(4);
+
+        for (int i=0;i<intList.size();i++){
+            System.out.println(i + ": " + intList.get(i));
+        }
+
     }
 }
